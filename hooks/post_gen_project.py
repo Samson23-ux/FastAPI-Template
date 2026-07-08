4da00578495b6b49bba2484a4bf63e5c5a0db035
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-07-08 10:55:38 +0100

    fix: write logic to remove .jinja extension

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,18 @@
+import os
 import subprocess
 from pathlib import Path
 
 
 def main() -> None:
+    root = "."
     project_dir = Path.cwd()
+
+    for dirpath, _, filenames in os.walk(root):
+        for filename in filenames:
+            if filename.endswith(".jinja"):
+                old_path = os.path.join(dirpath, filename)
+                new_path = os.path.join(dirpath, filename[: -len(".jinja")])
+                os.rename(old_path, new_path)
 
     if "{{ cookiecutter.make_http_requests }}" == "no":
         path = project_dir / "app" / "api" / "services" / "request.py"
