df4eb626f125c1fe709e0a38e8d97d140c5df332
Samson Ajifowoke <samsonajifowoke@gmail.com>
2026-07-08 09:30:13 +0100

    Initial commit

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+from pathlib import Path
+
+if "{{ cookiecutter.make_http_requests }}" == "no":
+    path = (
+        Path(__file__).parent
+        / "{{ cookiecutter.project_name }}"
+        / "app"
+        / "api"
+        / "services"
+        / "request.py"
+    )
+    os.remove(path)
+
+subprocess.run(["uv" "sync"])
